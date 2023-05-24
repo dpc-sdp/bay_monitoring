1392b094227ac1f1c1a60216ffca603181aab4fb
FleetAdmiralButter <ming.quah@protonmail.com>
2023-05-24 10:38:40 +1000

    [DDS-72] Remove plugins

--- src/Plugin/monitoring/Support/TideExternalServiceSupportInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\bay_monitoring\Plugin\monitoring\Support;
-
-use Drupal\monitoring\Result\SensorResultInterface;
-
-interface TideExternalServiceSupportInterface {
-  public function runCheck(SensorResultInterface &$result);
-}
